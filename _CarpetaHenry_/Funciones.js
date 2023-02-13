f2b26e668cf30b0aab58f5cc818e74756e4448d2
Gfernandez <giancarlo_fernandez1@usmp.pe>
2023-02-13 10:40:42 -0500

    Funciones

---
+++ _CarpetaHenry_/Funciones.js
@@ -0,0 +1,35 @@
+//function sumatres(x) {
+//    console.log(x+3);
+//}
+
+//sumatres(10);
+
+//Funcion 1
+//function sumaTres(x) {
+//    return x+3;
+//};
+
+//Funcion 2
+//var sumaTres = function (x) {
+//    return x+3;
+//};
+
+//Funcion 3
+//var sumaTres = (x) => {
+//    return x+3;
+//};
+
+
+
+
+
+function cuidadoconelconsolelog(nombre){
+    console.log(nombre);
+}
+
+function otrafuncion(){
+    return (
+        "El nombre por la funcion 'cuidadoconsolelog' es: "+
+        cuidadoconelconsolelog('Camilo')
+    );
+}
